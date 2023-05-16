10877efd49367742ab7c23d51755d4e3a09ae524
Marcelo Palacios <jpdjcp@gmail.com>
2023-05-16 13:07:36 -0300

    Create Incompatibilidad.java

---
+++ src/objetos/Incompatibilidad.java
@@ -0,0 +1,36 @@
+package objetos;
+
+public class Incompatibilidad
+{
+	private int id1;
+	private int id2;
+	
+	public Incompatibilidad(int id1, int id2)
+	{
+		setId1(id1);
+		setId2(id2);
+	}
+	
+	public int getId1()
+	{
+		return id1;
+	}
+	
+	public void setId1(int id1)
+	{
+		this.id1 = id1;
+	}
+	
+	public int getId2()
+	{
+		return id2;
+	}
+	
+	public void setId2(int id2)
+	{
+		this.id2 = id2;
+	}
+	
+	
+	
+}
