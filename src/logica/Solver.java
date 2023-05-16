7cfb0cfc05884575e9bc30618357fe470719a452
Marcelo Palacios <jpdjcp@gmail.com>
2023-05-16 11:49:20 -0300

    Create Solver.java
    Creada clase Solver con parte de la lógica de la app

---
+++ src/logica/Solver.java
@@ -0,0 +1,31 @@
+package logica;
+
+import java.util.List;
+import objetos.Persona;
+
+public class Solver
+{
+	private List<Persona> listaPersonas;
+//	private List<> incompatibles;
+//	private Requerimientos requerimientos;
+	
+	public void registrarPersona()
+	{
+		
+	}
+	
+	public void registrarIncompatibilidad()
+	{
+		
+	}
+	
+	public void registrarRequerimientos()
+	{
+		
+	}
+	
+	public List<Persona> generarMejorEquipo()
+	{
+		throw new RuntimeException("Método no implementado");
+	}
+}
