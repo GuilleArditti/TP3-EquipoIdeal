1c9c01ad418875857088f7113ced06689e0171ee
Marcelo Palacios <jpdjcp@gmail.com>
2023-06-02 00:46:04 -0300

    Update Solver.java
    Se incluye el opcional de las estadísticas (cantidad de llamadas recursivas)

--- src/grupoMejorCalificado/Solver.java
+++ src/grupoMejorCalificado/Solver.java
@@ -6,15 +6,14 @@ import objetos.Grupo;
 import objetos.Persona;
 import objetos.Requerimiento;
 
-public class Solver extends Thread
-{
+public class Solver extends Thread {
 	private List<Persona> personas;
 	private List<List<Integer>> incompatibles;
 	private Requerimiento requeridos;
 	private Grupo actual;
 	private Grupo grupoMayorPuntuacion;
+	private int cantRecursiones;
 
-	
 	public Solver(List<Persona> personas, List<List<Integer>> incompatibles, Requerimiento requeridos) {
 		setPersonas(personas);
 		setIncompatibles(incompatibles);
@@ -22,76 +21,76 @@ public class Solver extends Thread
 	}
 
 	@Override
-	public void run()
-	{
+	public void run() {
 		generarMejorEquipo();
 	}
-	
-	public void generarMejorEquipo()
-	{
+
+	public void generarMejorEquipo() {
 		actual = new Grupo();
 		grupoMayorPuntuacion = new Grupo();
+		setCantRecursiones(1);
 		generarDesde(0);
 	}
-	
-	private void generarDesde(int id)
-	{
-		// caso base 1 : llegamos a una hoja o el grupo actual tiene el tamanio requerido
-		if (id == personas.size() || actual.getTamano() == requeridos.getTamano())
-		{
+
+	private void generarDesde(int id) {
+		// caso base 1 : llegamos a una hoja o el grupo actual tiene el tamanio
+		// requerido
+		if (id == personas.size() || actual.getTamano() == requeridos.getTamano()) {
 			if (cumpleTodosRequisitos() && actual.getPuntuacion() > grupoMayorPuntuacion.getPuntuacion())
 				grupoMayorPuntuacion.clonar(actual);
 			return;
 		}
-		
-		// caso base 2 : si hay conflicto entre la persona a agregar y las que ya están en actual
+
+		// caso base 2 : si hay conflicto entre la persona a agregar y las que ya estan
+		// en actual
 		if (hayConflicto(id, actual.getPersonas()))
 			return;
-			
+
 		actual.agregar(personas.get(id));
-		generarDesde(id+1);
-			
+		cantRecursiones++;
+		generarDesde(id + 1);
+		
 		actual.quitar(personas.get(id));
-		generarDesde(id+1);
+		cantRecursiones++;
+		generarDesde(id + 1);
 	}
-	
-	private boolean cumpleTodosRequisitos()
-	{
+
+	private boolean cumpleTodosRequisitos() {
 		return actual.getCantLiderProyecto() == requeridos.getCantLiderProyecto()
 				&& actual.getCantArquitectos() == requeridos.getCantArquitectos()
 				&& actual.getCantDevelopers() == requeridos.getCantDevelopers()
 				&& actual.getCantTesters() == requeridos.getCantTesters();
 	}
-	
-	private boolean hayConflicto(int id, Set<Persona> personas)
-	{
+
+	private boolean hayConflicto(int id, Set<Persona> personas) {
 		for (Persona persona : personas)
 			if (incompatibles.get(id).contains(persona.getId()))
 				return true;
-			
+
 		return false;
 	}
-	
 
-
-	public void setPersonas(List<Persona> personas)
-	{
+	public void setPersonas(List<Persona> personas) {
 		this.personas = personas;
 	}
 
-	public void setIncompatibles(List<List<Integer>> incompatibles)
-	{
+	public void setIncompatibles(List<List<Integer>> incompatibles) {
 		this.incompatibles = incompatibles;
 	}
-	
 
-	public void setRequeridos(Requerimiento requeridos)
-	{
+	public void setRequeridos(Requerimiento requeridos) {
 		this.requeridos = requeridos;
 	}
 
-	public Grupo getGrupoMayorPuntuacion()
-	{
+	public Grupo getGrupoMayorPuntuacion() {
 		return grupoMayorPuntuacion;
+	}
+
+	private void setCantRecursiones(int n) {
+		this.cantRecursiones = n;
+	}
+	
+	public int getCantRecursiones() {
+		return cantRecursiones;
 	}
 }
