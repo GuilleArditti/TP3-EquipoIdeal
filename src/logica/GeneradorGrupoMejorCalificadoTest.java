6a591f0385fc23ca9f15dcc67ff58d2d28310a1a
Marcelo Palacios <jpdjcp@gmail.com>
2023-06-01 21:22:40 -0300

    Add test heuristica

--- src/logica/GeneradorGrupoMejorCalificadoTest.java
+++ src/logica/GeneradorGrupoMejorCalificadoTest.java
@@ -11,13 +11,12 @@ import objetos.Persona;
 
 public class GeneradorGrupoMejorCalificadoTest {
 	private GeneradorGrupoMejorCalificado generador;
-	
 
 	@Before
 	public void setup() {
 		generador = new GeneradorGrupoMejorCalificado();
 	}
-	
+
 	@Test
 	public void agregarPersonaTest() {
 		generador.agregarPersona(5, "Juan Perez", Rol.PROGRAMADOR);
@@ -33,44 +32,58 @@ public class GeneradorGrupoMejorCalificadoTest {
 		assertTrue(generador.getIncompatibilidadesById(0).contains(1));
 		assertTrue(generador.getIncompatibilidadesById(1).contains(0));
 	}
-	
+
 	@Test
 	public void generarMejorEquipoTest() {
 		armarGrupo();
-		int rendimientoEsperado=32;
-		int rendimientoObtenido=resultadoMejorEquipo();
-		assertEquals(rendimientoEsperado,rendimientoObtenido);
+		int rendimientoEsperado = 32;
+		int rendimientoObtenido = resultadoMejorEquipo();
+		assertEquals(rendimientoEsperado, rendimientoObtenido);
+	}
+
+	@Test
+	public void generarMejorEquipoHeuristicoTest() {
+		int rendimientoEsperado = 32;
+		int rendimientoObtenido = 0;
+		
+		armarGrupo();
+		Set<Persona> resultado = generador.generarMejorEquipoHeuristico();
+		System.out.println(resultado);
+		for (Persona persona : resultado)
+			rendimientoObtenido += persona.getRendimiento();
+		
+		assertEquals(rendimientoEsperado, rendimientoObtenido);
 	}
 
 	private int resultadoMejorEquipo() {
 		Set<Persona> resultado = generador.generarMejorEquipo();
-		
+
 		int rendimientoGlobal = 0;
-		
+
 		for (Persona persona : resultado)
 			rendimientoGlobal += persona.getRendimiento();
-		
+
 		return rendimientoGlobal;
 	}
 
 	private void armarGrupo() {
-		generador.agregarPersona(4, "Juan Perez", Rol.LIDER_DE_PROYECTO);
-		generador.agregarPersona(5, "Roberto Gomez", Rol.LIDER_DE_PROYECTO);
-		
-		generador.agregarPersona(3, "Micaela Robles", Rol.ARQUITECTO);
-		generador.agregarPersona(4, "Marcelo Rodriguez", Rol.ARQUITECTO);
-		generador.agregarPersona(5, "Esteban Quito", Rol.ARQUITECTO);
-		
-		generador.agregarPersona(3, "Braian Davico", Rol.PROGRAMADOR);
-		generador.agregarPersona(4, "Bruno Avila", Rol.PROGRAMADOR);
-		generador.agregarPersona(5, "Norberto Beltrán", Rol.PROGRAMADOR);
-		
-		generador.agregarPersona(5, "Natalia Infante", Rol.TESTER);
-		generador.agregarPersona(5, "Romina Herrera", Rol.TESTER);
-		generador.agregarPersona(4, "Miguel Britez", Rol.TESTER);
-		
+		generador.agregarPersona(4, "Lider1", Rol.LIDER_DE_PROYECTO);
+		generador.agregarPersona(5, "Lider2", Rol.LIDER_DE_PROYECTO);
+
+		generador.agregarPersona(3, "Arquitecto1", Rol.ARQUITECTO);
+		generador.agregarPersona(4, "Arquitecto2", Rol.ARQUITECTO);
+		generador.agregarPersona(5, "Arquitecto3", Rol.ARQUITECTO);
+
+		generador.agregarPersona(3, "Dev1", Rol.PROGRAMADOR);
+		generador.agregarPersona(4, "Dev2", Rol.PROGRAMADOR);
+		generador.agregarPersona(5, "Dev3", Rol.PROGRAMADOR);
+
+		generador.agregarPersona(5, "Tester1", Rol.TESTER);
+		generador.agregarPersona(1, "Tester2", Rol.TESTER);
+		generador.agregarPersona(4, "Tester3", Rol.TESTER);
+
 		generador.agregarIncompatibilidad(8, 9);
-		
+
 		generador.setRequerimientos(1, 2, 2, 2);
 	}
 }
